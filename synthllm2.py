110bdbd43b2c77ea9816cbdada932a57a35b9a74
harvind-here <harvindmukhal3@gmail.com>
2025-01-27 17:44:58 +0530

    using llama3.3

--- synthllm2.py
+++ synthllm2.py
@@ -44,7 +44,7 @@ Ensure that your output is a well-formatted JSON instance that conforms to the s
 )
 
 llm = ChatGroq(
-    model="llama-3.1-70b-versatile",
+    model="llama-3.3-70b-versatile",
     temperature=0.7,
     max_tokens=None,
     timeout=None,
